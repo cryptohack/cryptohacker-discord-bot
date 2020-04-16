93f063e10c2b435e83bf6fd2cb7c347426391a8b
Robin Jadoul <robin.jadoul@gmail.com>
2020-04-16 20:57:03 +0200

    Is this more robust than data?

--- fun.py
+++ fun.py
@@ -4,4 +4,4 @@ from xml.dom import minidom
 def get_bruce_fact():
     raw = requests.get("https://www.schneierfacts.com/rss/random").text
     dom = minidom.parseString(raw)
-    return dom.getElementsByTagName("description")[1].firstChild.data
+    return dom.getElementsByTagName("description")[1].firstChild.nodeValue
